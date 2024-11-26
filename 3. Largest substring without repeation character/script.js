b0221b1660becf4dbf26ac62336c64b8852c8eab
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-11-26 16:50:08 +0530

    finished largest substring without repeating character

--- 3. Largest substring without repeation character/script.js
+++ 3. Largest substring without repeation character/script.js
@@ -1,1 +1,17 @@
-console.log("lorgest substring ");
+
+s=prompt("Enter the largest string");
+let largestString=function(val){
+                
+   let set=new Set()
+   let arr=[]
+  for(sdata of val){ 
+      set.add(sdata)
+    }
+    for(result of set.keys()){
+        arr.push(result)   
+    }
+    console.log(arr.length);
+} 
+
+largestString(s) 
+
