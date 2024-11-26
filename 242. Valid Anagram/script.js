5102065483e189f3880efe9c88243c3eb7c0bb15
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-11-26 16:03:44 +0530

    80% finished the Valid Anagram

--- 242. Valid Anagram/script.js
+++ 242. Valid Anagram/script.js
@@ -1,1 +1,29 @@
-console.log("Hello world ");
+let s=prompt("1")
+let t=prompt("2")
+let i=0;
+
+let data=new Map()
+
+const myfunction =function(){
+    if(s.length !=t.length){
+        return false
+    }
+    for(let sdata of s){ 
+        data.set(sdata,i);
+        i++  
+   }
+    for(let tdata of t){  
+         if(data.has(tdata)){ 
+         return true 
+         }else{
+          return  false 
+         }
+      }
+} 
+let functionData=myfunction()
+console.log(functionData);
+
+
+
+
+
