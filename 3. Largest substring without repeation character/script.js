2a3d3b902ae77150f0aa30f56f8e7a87df2707b3
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-11-28 13:05:33 +0530

    wirte optimize code in question No. 3

--- 3. Largest substring without repeation character/script.js
+++ 3. Largest substring without repeation character/script.js
@@ -1,17 +1,29 @@
-
-s=prompt("Enter the largest string");
-let largestString=function(val){
+                    // Type - 1 Solved 
+// s=prompt("Enter the largest string");
+// let largestString=function(val){
                 
-   let set=new Set()
-   let arr=[]
-  for(sdata of val){ 
-      set.add(sdata)
-    }
-    for(result of set.keys()){
-        arr.push(result)   
-    }
-    console.log(arr.length);
-} 
+//    let set=new Set()
+//    let arr=[]
+//   for(sdata of val){ 
+//       set.add(sdata)
+//     }
+//     for(result of set.keys()){
+//         arr.push(result)   
+//     }
+//     console.log(arr.length);
+// } 
 
-largestString(s) 
+// largestString(s) 
+
+
+                // Type - 2 Optimize
+const  sdata="abcacdcdc"
+let set=new Set()
+for( val of sdata ){
+     set.add(val)
+    
+}
+
+let arr=Array.from(set) 
+console.log(arr.length);
 
