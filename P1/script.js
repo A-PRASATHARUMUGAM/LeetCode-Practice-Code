1b5fe19f0f397164fc37028b42914b5d22218d6a
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2025-02-27 00:37:25 +0530

    I am back

---
+++ P1/script.js
@@ -0,0 +1,38 @@
+const originalString="prasath";
+
+function reverseString(){
+  
+
+    
+}
+
+reverseString()
+let array=[1,2,3]
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(element)
+   }
+
+// let result=[] 
+// for(i=0;i<originalString.length;i++){
+//     result.push(i)
+//     console.log(result);
+// }   
+
+
+
+// const arrval=[1,2,3];
+                    
+//  for(i=0;i<arrval.length;i++){
+     
+//      let result=arrval[i];
+    
+//  }
+
+
+//  for( val of arrval){
+   
+//    console.log(val)
+    
+//  }
