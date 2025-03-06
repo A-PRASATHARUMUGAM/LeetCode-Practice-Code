893082e34c3ec195679a8ac946f5e2baa3765af8
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2025-03-06 12:03:49 +0530

    create react app

---
+++ react_practice/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+   
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>I am Prasath</p>
+<a className="App-link" href="" target="_blank" rel="noopener noreferrer">Hello World I am back</a>
+      </header>
+
+    </div> 
+  );
+}
+
+export default App;
