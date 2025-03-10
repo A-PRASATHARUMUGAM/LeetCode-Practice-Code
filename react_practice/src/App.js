a790d73e33fe024d19583c7de435d2363f03e179
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2025-03-10 21:26:20 +0530

    finished the problem in react

--- react_practice/src/App.js
+++ react_practice/src/App.js
@@ -2,20 +2,27 @@ import './App.css';
 
 function app() {
 
-  const array=["Earn","Grow","Give"]   
-   
-  for(val of array ){
-
-    console.log(val);
+  let array=["Earn","Grow","Give"]   
+  let result="";
+  for(let i=0;i<array.length; i++){
+    let rand=Math.floor(Math.random()*array.length);
+    result=array[rand];
     
   }
-   
+
+      
+  
   return (
    <div className="app">
-    <h1>Let's {array} Money</h1>
+    <h1>Let's {result} Money</h1>
 
-   </div>
+   </div> 
+
+
   );
+
+
+
 }
 
 export default app;
--- react_practice/src/index.js
+++ react_practice/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'; 
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -6,8 +6,8 @@ import Comments from './Comments';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode> 
-    <App />
+  <React.StrictMode>  
+    <App />  
     <Comments/>
   </React.StrictMode>
 );
