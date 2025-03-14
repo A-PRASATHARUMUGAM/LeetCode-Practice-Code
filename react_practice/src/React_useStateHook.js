3b4758559127ccb84afd7b68eab9e8d875207447
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2025-03-15 01:04:52 +0530

    change some shorter

--- react_practice/src/React_useStateHook.js
+++ react_practice/src/React_useStateHook.js
@@ -8,40 +8,34 @@ import { useState } from 'react'
 
 const React_useStateHook=()=>{
 
-   
+       // Third - 3
+    /* name  -> open website stage  */
+    /* setName  -> use to change  website stage  */
+    const [name,setName]=useState("change name");
+
+
+
+
     // second - 2
     const myfunction =()=>{
         
         let array=["prasath","vignesh","sabari"];
         let rand=Math.floor(Math.random()*array.length);
-        return array[rand]; 
-        
-        
+        setName(array[rand]); 
+    
     }
-    // Third - 3
-    /* name  -> open website stage  */
-    /* setName  -> use to change  website stage  */
-    const [name,setName]=useState("change name");
+
     
     
-
-
-    //fourth - 4
-    const  handleClick=()=>{ 
-        setName(myfunction())
-      }
- 
-
-
     return(
             <div className="react-userstate-container">
                 <h1>React useState Hook</h1>
 
 
-
-                {/* fifth - 5 */}
+ 
+                {/* fourth - 4 */}
                 <p>Hello {name} </p> 
-                <button onClick={()=>handleClick()}>Click</button> 
+                <button onClick={()=>myfunction()}>Click</button> 
 
             </div>
 
