27f052c83c2584872c08ccd146c41c8692b876ab
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2025-03-14 22:46:29 +0530

    Practice Styles in react

--- react_practice/src/React_Styles.js
+++ react_practice/src/React_Styles.js
@@ -1,11 +1,27 @@
 import React from 'react'
 
+// External style
+import "./index.css";
+
+
 const React_Styles = () => {
+
+// variable to declare style 
+const internalstyle={ color:"red"} 
+
   return (
 
     <div className="style-main-container">
+      <h1> Styles In React</h1>
+
+
+    {/* variable to declare style   */}
+      <h3 style={internalstyle}>Internal Style</h3>
+
       
-      <h1></h1>
+    {/* Inline style  */}
+      <h3 style={{color:"blue"}} >Inline Style</h3>
+      
       
       
       </div>
