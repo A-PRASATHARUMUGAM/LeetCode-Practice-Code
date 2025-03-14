6d490255ea4069fa4e441ebe36045382fd3c530c
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2025-03-15 01:01:17 +0530

    Practice finished in react hook

--- react_practice/src/React_useStateHook.js
+++ react_practice/src/React_useStateHook.js
@@ -1,9 +1,36 @@
 import React from 'react'
-
+   
+// first - 1
 // Hook import to " curley brasses "
 import { useState } from 'react' 
 
+
+
 const React_useStateHook=()=>{
+
+   
+    // second - 2
+    const myfunction =()=>{
+        
+        let array=["prasath","vignesh","sabari"];
+        let rand=Math.floor(Math.random()*array.length);
+        return array[rand]; 
+        
+        
+    }
+    // Third - 3
+    /* name  -> open website stage  */
+    /* setName  -> use to change  website stage  */
+    const [name,setName]=useState("change name");
+    
+    
+
+
+    //fourth - 4
+    const  handleClick=()=>{ 
+        setName(myfunction())
+      }
+ 
 
 
     return(
@@ -11,9 +38,14 @@ const React_useStateHook=()=>{
                 <h1>React useState Hook</h1>
 
 
+
+                {/* fifth - 5 */}
+                <p>Hello {name} </p> 
+                <button onClick={()=>handleClick()}>Click</button> 
+
             </div>
 
-
+ 
 
     )
 
