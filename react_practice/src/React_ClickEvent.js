713c27c616f04ad16ca4a20c66236ac59428f7bb
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2025-03-14 23:47:29 +0530

    finished React click event

--- react_practice/src/React_ClickEvent.js
+++ react_practice/src/React_ClickEvent.js
@@ -1,7 +1,21 @@
+import { click } from '@testing-library/user-event/dist/click';
 import React from 'react'
 
 const React_ClickEvent = () => {
 
+    // Type 1 - Normal 
+const handleClick =()=>{
+        console.log("click 1"); 
+        
+    }
+
+
+
+    // Type 2  - Parameter Passing 
+const handleClick2 =(fname)=>{
+        console.log(`click 2 ${fname}`); 
+        
+    }
 
 
 
@@ -11,10 +25,16 @@ const React_ClickEvent = () => {
 
         <h1>React Click Events</h1>
 
-        <button >Submit</button>
 
+        {/* Type 1 - Normal */}
+        <button className='rcbutton' onClick={handleClick}>Click 1</button>
 
+    
+        {/* Type 2  - Parameter Passing */} 
+        <button className='rcbutton'  onClick={() => handleClick2("Prasath")}>Click 2</button>
 
+                    
+ 
 
     </div>
   )
