cfce37e700cda577a97f8fc42cc0eb3e4e8bacaf
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2025-03-15 20:58:55 +0530

    finshed find the largest element in array

--- Array Problems/Largest element in Array/script.js
+++ Array Problems/Largest element in Array/script.js
@@ -1,1 +1,19 @@
-console.log("hello world ");
+
+const myfunction=(arr)=>{
+
+    let  max=arr[0];
+
+for(i=1;i<arr.length;i++){
+ 
+    if(arr[i]<max){
+          max=arr[i]
+    }
+ 
+}
+console.log(max);
+
+    
+}
+
+
+myfunction([3,3,4,5,6,9])
