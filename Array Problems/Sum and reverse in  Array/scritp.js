c6f66e5d6f769991b6449aea22646b0a4d7f9283
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2025-03-17 20:08:55 +0530

    space to organice the code

--- Array Problems/Sum and reverse in  Array/scritp.js
+++ Array Problems/Sum and reverse in  Array/scritp.js
@@ -1,39 +1,28 @@
 // Sum the Array
 let arr=[10,10,5,5];
-let sum=0;
-for(i=0;i<arr.length;i++){
-
-    sum=arr[i]
-    
+let sum = 0;
+for(i = 0; i < arr.length; i++){
+    sum = arr[i]
 }
 
 console.log(sum);
 
 
-
-
 // Reverse Array  1
 
-let arr2=[1,2,3,4,5,8];
+let arr2 = [1,2,3,4,5,8];
 
-let reverse=arr2.reverse()
+let reverse =  arr2.reverse()
 
 console.log(reverse);
 
 
-
 // Reverse Array  2
 
-
-
-let arr3=[1,2,3,4,5,6,7,8]
-
-let result=[];
-
-for(i=arr3.length -1;i>=0;i--){
-
+let arr3 = [1,2,3,4,5,6,7,8]
+let result = [];
+for(i=arr3.length -1; i >= 0; i--){
         result.push(arr3[i])
-
 }
 
 console.log(arr3);
