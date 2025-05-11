6eebc0468348ef81932afc8c989e3cc27cc4856a
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2025-05-11 15:04:13 +0530

    finished the react click evetns

--- react_practice/react-all-files/src/React_clickEvents.js
+++ react_practice/react-all-files/src/React_clickEvents.js
@@ -1,12 +1,24 @@
 import React from 'react'
 
 const React_clickEvents = () => {
-  return (
-    <div className='React-clickEvents major-style'>
 
+  // Handle click Events
+   let handleClick=(e)=>{
+     e.target.classList.toggle('clickstyle');
+
+ 
+   }
+  
+  
+
+
+
+  return (
+    <div className='react-clickevents major-style'>
              <h1>React ClickEvents </h1>
+         <button className='h' onClick={(e)=>handleClick(e)} >Click Me</button>
     </div>
-  )
+  ) 
 }
 
 export default React_clickEvents  
