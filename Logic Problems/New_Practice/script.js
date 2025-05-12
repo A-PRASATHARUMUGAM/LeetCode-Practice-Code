d4c4aec3db5e4afc9516f7e3c661834f0954b9ea
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2025-05-12 22:44:02 +0530

    add some comment and problem

--- Logic Problems/New_Practice/script.js
+++ Logic Problems/New_Practice/script.js
@@ -1,29 +1,36 @@
+// Solved Problems
 
 
-let arr=[1,2,3,4,5,6];
+            // ARRAY
+// 1. Array max and min
+// 2. Array smallest and largest element
+// 3. Array reversed 
+// 4. odd and even number using array 
+// 5. minimum difference in array 
 
-for(val of arr){
+
+
+            // STRING
+// 1. String reversed 
+// 2. String Palindrom or not 
+// 3. Prime number 
+// 4. Find the largest substring without repeating character 
+
+
+let arr=[30, 5, 20, 9];
+let val1=5;
+let val2=9;
+let resultfinal;
+for(let i=0;i<arr.length;i++){
     
+if(arr[i]===val1&&val2){
 
+console.log(Math.abs(val1-val2));
+
+    
+} 
 
 }
 
+// console.log(resultfinal); 
 
-
-
-
-
-
-
-
-
-
-   
-
-
-
-// if( a%1==0 ){
-//     console.log(a);   
-// }else{
-//     console.log(b);
-// }
