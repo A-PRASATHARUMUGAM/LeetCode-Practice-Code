29d1228b153f220336cd676499373ae1626442e3
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2025-05-15 03:22:58 +0530

    add ReactListAndKeys component

---
+++ react_practice/react-all-files/src/React_List_and_Keys.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+function ReactListAndKeys(){
+
+
+    return(
+        <div className='reactlistandkeys'>
+   <h1>react list and keys</h1>
+
+
+        </div>
+    )
+
+
+
+}
+
+export default ReactListAndKeys
